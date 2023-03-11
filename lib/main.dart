e77f6c14207225c035ef11e4c8668445251176ea
BouchraBenGhazala <ghazalabouchra@gmail.com>
2023-03-11 17:06:01 +0100

    bedelt salam

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MainApp extends StatelessWidget {
     return const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('S A L A M'),
+          child: Text('SALAM.'),
         ),
       ),
     );
