46b7fd6e5f649addb0f7543d17c0685621373201
narogishin <94650561+narogishin@users.noreply.github.com>
2023-03-11 16:12:18 +0100

    bedelto b salam

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MainApp extends StatelessWidget {
     return const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('SALAM'),
+          child: Text('S A L A M'),
         ),
       ),
     );
