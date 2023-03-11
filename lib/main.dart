df3bcf653a863b46f59a33046f2ae8bb21a9444d
narogishin <94650561+narogishin@users.noreply.github.com>
2023-03-11 17:53:41 +0100

    app Bar error

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       home: Scaffold(
+        appBar: AppBar(),
         body: Center(
           child: Text('SALAM.'),
         ),
